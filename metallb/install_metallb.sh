67d048a4b6534c966e0065d2416ac84986fccbde
Fredrik Hansen <fredrik.hansen@eve.sx>
2022-06-08 15:26:06 +0200

    first commit

---
+++ metallb/install_metallb.sh
@@ -0,0 +1,7 @@
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+kubectl apply -f 1.yml
+kubectl apply -f 2.yml
+
