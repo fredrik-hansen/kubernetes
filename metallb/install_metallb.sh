8d3906c0f432e87eda2e30b491483631a0375c5c
fredrik-hansen <98120861+fredrik-hansen@users.noreply.github.com>
2022-06-13 13:10:02 +0200

    Update install_metallb.sh

--- metallb/install_metallb.sh
+++ metallb/install_metallb.sh
@@ -1,3 +1,6 @@
+#Check for info https://www.reddit.com/r/homelab/comments/ipsc4r/howto_k8s_metallb_and_external_dns_access_for/
+
+
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
